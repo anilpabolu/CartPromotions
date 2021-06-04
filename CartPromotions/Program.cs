f6df6b6886864a0bd7c64b274d91c3227a3d8f38
Anil P <anilpabolu.mastek@gmail.com>
2021-06-04 21:30:22 +0530

    Added Changes to Console application to show the products from the json config list

--- CartPromotions/Program.cs
+++ CartPromotions/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PromotionEngine;
+using static System.Console;
 
 namespace CartPromotions
 {
@@ -10,6 +7,19 @@ namespace CartPromotions
     {
         static void Main(string[] args)
         {
+            var productList = ConfigDetails.GetAvailableProducts();
+           
+
+            WriteLine("\t\t\t *** Welcome to Product Catalogue Page ***");
+            WriteLine("\t\t\t -----------------------------------------\n\n");
+            //WriteLine("\n\n");
+            WriteLine("SKU\t\t Product\t\t Price");
+            WriteLine("---------------------------------------------------");
+            foreach (var item in productList) WriteLine(item.Sku + "\t\t" + item.ProductName + "\t\t" + item.Price);
+
+            WriteLine("\n");
+
+
         }
     }
 }
