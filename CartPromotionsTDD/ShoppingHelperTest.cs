e1450c320abadc244ef15b5b47cebf9f15810d25
Anil P <anilpabolu.mastek@gmail.com>
2021-06-04 22:39:16 +0530

    Added Test methods

---
+++ CartPromotionsTDD/ShoppingHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PromotionEngine.Database;
+using PromotionEngine.Helper;
+using PromotionEngine.Models;
+
+
+namespace CartPromotionsTDD
+{
+    class ShoppingHelperTest
+    {
+        [Test]
+        public void GetPromotionlOffersTest()
+        {
+
+            IConfigDetails<Product> getConfig = new Products();
+            var productList = getConfig.Get();
+
+            var actualResult = ShoppingHelper.DoShopping(productList, "A", 2);
+
+            var expectedResult = new List<Product>
+            {
+                new Product{Sku= "A", ProductName= "ProductA",Price =50 },
+                new Product { Sku = "A", ProductName = "ProductA", Price = 50 }
+            };
+
+            Assert.AreEqual(true, actualResult.Item2);
+
+        }
+    }
+}
